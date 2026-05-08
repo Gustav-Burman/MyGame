f0350d403e5559398fe98ae12338da1ed43ba9d4
Gustav <gustavburman27@gmail.com>
2026-05-08 13:00:05 +0200

    Setting up file structure and cmake

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+﻿// game.cpp : Defines the entry point for the application.
+//
+
+#include "main.h"
+#include <SDL3/SDL.h>
+
+int main()
+{
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
